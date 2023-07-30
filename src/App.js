e120151e2950c0ac1b2af15a5ac5a602950fdbbd
chaabanikamel <chaabanik48@gmail.com>
2023-07-30 02:06:09 +0100

    Using rray As State in The Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
-import MyInput from './MyInput';
-import sideMenu from "./SideMenu";
-import MyForms from './MyForms';
-import Scoreboard from './Scoreboard';
+import { useState } from 'react';
 function App(){
+  const [deviceNameInputValue,setDeviceNameInputValue]= useState("")
+  const [devices,setDevices] = useState(["iphone","mac","samsung","windows"])
+  const devicesList= devices.map((device)=>{
+    return <li>{device}</li>
+  })
+  function handelAddClick(){
+    // const newDevices =[...devices];
+    // newDevices.push(deviceNameInputValue)
+    // setDevices(newDevices)
+    setDevices([...devices,deviceNameInputValue])
+  }
+
   return(
     <div className = "App" style={{paddingTop:"300px" }}>
       {/* <Header/>
       <MyForms/> */}
-        <Scoreboard/>
+        {/* <Scoreboard/> */}
+        {devicesList}
+        <label >Device Name:</label>
+        <input value= {deviceNameInputValue} onChange={(event)=>{setDeviceNameInputValue(event.target.value)}}type="text" />
+        <button onClick={handelAddClick}onChange={(=>{})}>Add</button>
     </div>
 }
  
